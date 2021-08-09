75b4672c09e8e99a18146350cb74e7dd0513e63a
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2021-08-09 18:02:42 +0300

    made include file and rebuild views

--- englishdictionary/dictionary/urls.py
+++ englishdictionary/dictionary/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from dictionary.views import IndexView
+from dictionary.views import IndexView, WordFindView
 app_name = "dictionary"
 
 urlpatterns = [
-    path('',IndexView.as_view(),name="index")
+    path('',IndexView.as_view(),name="index"),
+    path('word', WordFindView.as_view(),name="word")
 ]
--- englishdictionary/dictionary/views.py
+++ englishdictionary/dictionary/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from typing import get_args
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, request
 from django.views.generic import View
 
-import PyDictionary
+from PyDictionary import PyDictionary
 
 class IndexView(View):
     def get(self,request):
         return render(request, template_name='dictionary/index.html')
 
+class WordFindView(View):
+    def get(self,request,*args, **kwargs):
+        
+        unknown_word = request.GET.get("unknown-word")
+        dictionary = PyDictionary()
+        meaning = dictionary.meaning(unknown_word)
+        context = {
+            "meaning": meaning,
+        }
+        print(meaning)
+        return redirect("dictionary:index")
+        
+    
+        
