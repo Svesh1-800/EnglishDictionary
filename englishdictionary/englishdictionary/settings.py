cb69e5b70c7a0ef69aee919f9f2b0ba544b96f62
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2021-08-10 11:20:56 +0300

    connect to heroku

--- englishdictionary/englishdictionary/settings.py
+++ englishdictionary/englishdictionary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-)*_w5u72!^sgbba@lk^00smi&dy!8)!l)3-^w%-8uw6*egcd@y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,4 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
 
